379597f3938e4c353b9ff408ccf82ec7f98df226
Wojciech M. Zabolotny <wzab01@gmail.com>
2023-01-23 19:15:51 +0100

    Added wz_random solution.

---
+++ vhdl_urandom/wz_random_pkg.vhd
@@ -0,0 +1,50 @@
+-------------------------------------------------------------------------------
+-- Title      : Function returning random bytes from Linux /dev/urandom
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : wz_random_pkg.vhd
+-- Author     : Wojciech M. Zabolotny wzab<at>ise.pw.edu.pl
+-- Company    : 
+-- Created    : 2013-11-30
+-- Last update: 2015-02-04
+-- Platform   : 
+-- Standard   : VHDL'93
+-- License    : PUBLIC DOMAIN CODE
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2013 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2013-11-30  1.0      wzab    Created
+--
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library std;
+use std.textio.all;
+
+package wz_random_pkg is
+
+  function random_byte
+    return unsigned;
+
+end package wz_random_pkg;
+
+package body wz_random_pkg is
+
+  type T_CHAR_FILE is file of character;
+  file urandom_file : T_CHAR_FILE open read_mode is "/dev/urandom";
+
+  function random_byte
+    return unsigned is
+    variable res      : unsigned(7 downto 0);
+    variable rnd_char : character;
+  begin  -- function random_byte
+    read(urandom_file, rnd_char);
+    res := to_unsigned(character'pos(rnd_char), 8);
+    return res;
+  end function random_byte;
+
+end package body wz_random_pkg;
---
+++ vhdl_urandom/wz_random_tb.vhd
@@ -0,0 +1,63 @@
+-------------------------------------------------------------------------------
+-- Title      : Testbench for design "wz_random_pkg"
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : wz_random_tb.vhd
+-- Author     : Wojciech M. Zabolotny wzab<at>ise.pw.edu.pl
+-- Company    : 
+-- Created    : 2013-11-30
+-- Last update: 2015-02-04
+-- Platform   : 
+-- Standard   : VHDL'93
+-- License    : PUBLIC DOMAIN CODE
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2013 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2013-11-30  1.0      wzab    Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.wz_random_pkg.all;
+-------------------------------------------------------------------------------
+
+entity wz_random_tb is
+
+end wz_random_tb;
+
+-------------------------------------------------------------------------------
+
+architecture test1 of wz_random_tb is
+
+  -- clock
+  signal Clk : std_logic := '1';
+
+begin  -- test1
+
+  -- clock generation
+  Clk <= not Clk after 10 ns;
+
+  -- waveform generation
+  WaveGen_Proc : process
+    variable data : unsigned(7 downto 0);
+  begin
+    for i in 0 to 50 loop
+      wait until Clk = '0';
+      wait until Clk = '1';
+      data := random_byte;
+      report integer'image(to_integer(data)) severity note;
+    end loop;
+    wait;
+  end process WaveGen_Proc;
+
+
+end test1;
+
+
