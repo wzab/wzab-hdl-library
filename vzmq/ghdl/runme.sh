28bb3745e8a4a3c7e89c0e7fef1646951d104ef9
Wojciech Zabolotny <wzab@HPAIRWZ.lan>
2023-08-14 21:16:40 +0200

    Updated the shell and Python scripts for Python3. Added "-hold" flag to xterm so that the window does not disappear in case of errors.

--- vzmq/ghdl/runme.sh
+++ vzmq/ghdl/runme.sh
@@ -3,6 +3,6 @@ set -e
 gcc -c vzmqc.c 
 ghdl -a vzmq_pkg.vhd vzmq.vhd vzmq_tb.vhd 
 ghdl -e -Wl,vzmqc.o -Wl,-lzmq vzmq_tb
-xterm -e "sleep 1; python zmqtest.py" &
+xterm -hold -e "sleep 1; python3 zmqtest.py" &
 ./vzmq_tb --wave=test.ghw
 
